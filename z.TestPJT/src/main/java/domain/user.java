58ac9e153b0159d59a90664ed0f0731fc21d54c9
bitcamp <bitcamp@DESKTOP-OAPS5AL>
2022-09-30 11:20:46 +0900

    Create : 2022.09.30 ~~~~

---
+++ z.TestPJT/src/main/java/domain/user.java
@@ -0,0 +1,14 @@
+package domain;
+
+public class user {
+
+	public user() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ z.TestPJT/src/main/java/user/B.java
@@ -0,0 +1,9 @@
+package user;
+
+public class B {
+
+	public B() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
